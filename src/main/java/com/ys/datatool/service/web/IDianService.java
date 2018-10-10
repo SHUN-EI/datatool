acb952d0a500f33104d0bf2bae8209fab6e1b241
MoWeiQuan <295597613@qq.com>
2018-10-10 15:55:22 +0800

    i店-会员卡相关修改

--- src/main/java/com/ys/datatool/service/web/IDianService.java
+++ src/main/java/com/ys/datatool/service/web/IDianService.java
@@ -61,7 +61,15 @@ public class IDianService {
 
     private String companyName = "I店";
 
-    private String COOKIE = "JSESSIONID=42FC878D2B5EBD31B99C8523EFD2573D";
+    private String meid = "688FFA7A-C3C9-48A5-AD1C-EE41EE2FB1CF";
+
+    private String sign = "A5704DA99A3B9B8E74D284DAA62D8C22";
+
+    private String token = "F219929B88C2768BA6313F3063DC8014";
+
+    private String userPhone = "18924800202";
+
+    private String COOKIE = "JSESSIONID=7026F61FE1C588F8DA0ED88B0F9D7512";
 
 
     /**
@@ -86,7 +94,7 @@ public class IDianService {
          */
 
         if (total > 0) {
-            for (int i = 1; i <= 10; i++) {
+            for (int i = 1; i <= total; i++) {
                 res = ConnectionUtil.doGetEncode(BILL_URL + String.valueOf(i), COOKIE, ACCEPT_ENCODING, ACCEPT);
                 JsonNode content = MAPPER.readTree(res.returnContent().asString(charset));
 
@@ -266,8 +274,8 @@ public class IDianService {
     }
 
     /**
-     * 车辆信息
-     * 需要获取全部车牌号码，此方法需要读取所有车牌的excel
+     * 会员卡车辆信息
+     * 根据会员卡excel的车牌号这一列数据，去查询车辆信息详情
      *
      * @throws IOException
      */
@@ -276,7 +284,7 @@ public class IDianService {
         List<CarInfo> carInfos = new ArrayList<>();
         List<String> carNumbers = new ArrayList<>();
 
-        File file = new File("C:\\exportExcel\\icard.xls");
+        File file = new File("C:\\exportExcel\\i店会员卡.xlsx");
         FileInputStream in = new FileInputStream(file);
         HSSFWorkbook wb = new HSSFWorkbook(in);
 
@@ -288,7 +296,7 @@ public class IDianService {
         HSSFCell cell = null;
         for (int i = firstRowNum; i <= lastRowNum; i++) {
             row = sheet.getRow(i);//取得第i行
-            cell = row.getCell(0);//取得i行的第一列
+            cell = row.getCell(3);//取得i行的第四列车牌号
             String cellValue = cell.getStringCellValue().trim();
             carNumbers.add(cellValue);
         }
@@ -444,12 +452,12 @@ public class IDianService {
 
     private List<BasicNameValuePair> getMemberCardItemParams(String id) {
         List<BasicNameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("MEID", "1F3F7042-675B-4BAD-BE11-448A267326F0"));
+        params.add(new BasicNameValuePair("MEID", meid));
         params.add(new BasicNameValuePair("deviceType", "2"));
         params.add(new BasicNameValuePair("format", "json"));
-        params.add(new BasicNameValuePair("sign", "832C4564B8250546FB74DA4E11365984"));
-        params.add(new BasicNameValuePair("token", "A19873FDF327F6D7F14A8110513DB9F7"));
-        params.add(new BasicNameValuePair("user_phone", "18934388886"));
+        params.add(new BasicNameValuePair("sign", sign));
+        params.add(new BasicNameValuePair("token", token));
+        params.add(new BasicNameValuePair("user_phone", userPhone));
         params.add(new BasicNameValuePair("versionCode", "507"));
         params.add(new BasicNameValuePair("versionName", "5.0.7"));
         params.add(new BasicNameValuePair("memberId", id));
@@ -458,14 +466,14 @@ public class IDianService {
 
     private List<BasicNameValuePair> getMemberCardParams(String index) {
         List<BasicNameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("MEID", "1F3F7042-675B-4BAD-BE11-448A267326F0"));
+        params.add(new BasicNameValuePair("MEID", meid));
         params.add(new BasicNameValuePair("deviceType", "2"));
         params.add(new BasicNameValuePair("format", "json"));
         params.add(new BasicNameValuePair("keyword", ""));
         params.add(new BasicNameValuePair("memberLevelId", ""));
-        params.add(new BasicNameValuePair("sign", "1B0BC2BC981BF781DDB9D55FAA886D3E"));
-        params.add(new BasicNameValuePair("token", "A19873FDF327F6D7F14A8110513DB9F7"));
-        params.add(new BasicNameValuePair("user_phone", "18934388886"));
+        params.add(new BasicNameValuePair("sign", sign));
+        params.add(new BasicNameValuePair("token", token));
+        params.add(new BasicNameValuePair("user_phone", userPhone));
         params.add(new BasicNameValuePair("versionCode", "507"));
         params.add(new BasicNameValuePair("versionName", "5.0.7"));
         params.add(new BasicNameValuePair("currentPageIndex", index));
