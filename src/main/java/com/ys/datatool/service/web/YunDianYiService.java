1a30520f087d77d0c9ec0f6d2730fc7a39763454
MoWeiQuan <295597613@qq.com>
2018-12-06 21:43:02 +0800

    云店易系统-促销套餐数据(未完,cookie问题没解决)

--- src/main/java/com/ys/datatool/service/web/YunDianYiService.java
+++ src/main/java/com/ys/datatool/service/web/YunDianYiService.java
@@ -1,13 +1,21 @@
 package com.ys.datatool.service.web;
 
 import com.ys.datatool.domain.HtmlTag;
+import com.ys.datatool.domain.MemberCard;
+import com.ys.datatool.util.CommonUtil;
 import com.ys.datatool.util.ConnectionUtil;
 import com.ys.datatool.util.WebClientUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.fluent.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.Test;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by mo on @date  2018/12/5.
@@ -20,11 +28,13 @@ public class YunDianYiService {
 
     private String PACKAGECAR_URL = "https://vip.yundianyi.com/promotion/ajaxpackageshow/id/";
 
-    private String PACKAGEPAGE_URL = "https://vip.yundianyi.com/promotion/package/";
+    private String PACKAGEPAGE_URL = "https://vip.yundianyi.com/promotion/getpackagepage/type/enable/classid/1/page/";
 
-    private String PACKAGELIST_URL = "https://vip.yundianyi.com/promotion/getpackagepage/type/enable/page/";
+    private String PAGE_COOKIE = "PHPSESSID=5dgjhd202dv6qo0l0oq08o96u4; ace_settings=%7B%22sidebar-collapsed%22%3A-1%7D; SERVERID=02f09cca2cc8a5aa65c27d4c679ad17a|1544098808|1544098808";
 
-    private String COOKIE = "PHPSESSID=q04a28a91hbf8rciq0jusmp6g0; ace_settings=%7B%22sidebar-collapsed%22%3A1%7D; SERVERID=0bb7dc3a32edb4645acc8c025bd1047b|1544007351|1544007346";
+    private String serverId = "SERVERID=02f09cca2cc8a5aa65c27d4c679ad17a|1544098982|1544098982";
+
+    private String COOKIE = "PHPSESSID=5dgjhd202dv6qo0l0oq08o96u4; ace_settings=%7B%22sidebar-collapsed%22%3A-1%7D; " + serverId;
 
 
     /**
@@ -37,33 +47,63 @@ public class YunDianYiService {
      */
     @Test
     public void fetchMemberCardItemDataStandard() throws Exception {
+        Set<String> serverIds = new HashSet<>();
+        List<MemberCard> memberCards = new ArrayList<>();
 
-        Response response = ConnectionUtil.doGetWithLeastParams(PACKAGEPAGE_URL, COOKIE);
+        Response response = ConnectionUtil.doGetWithLeastParams(PACKAGEPAGE_URL + 1, PAGE_COOKIE);
         String html = response.returnContent().asString();
         Document document = Jsoup.parse(html);
 
-        String totalRegEx = "#enablePackage > div.text-center > ul > li:nth-child(3) > input";
+        String totalRegEx = "#package_page_table_enable_1 > table > tbody > tr > td > div > ul > li:nth-child(3) > input";
         String totalPageStr = document.select(totalRegEx).attr("data-max-page");
         int total = Integer.parseInt(totalPageStr);
 
         if (total > 0) {
-            for (int i = 1; i <= total; i++) {
+            //每页为10行，取多一页
+            for (int i = 1; i <= total * 11; i++) {
+                Response res = ConnectionUtil.doGetWithLeastParams(PACKAGEPAGE_URL + i, COOKIE);
+                String cookie = res.returnResponse().getFirstHeader("Set-Cookie").getValue();
+                cookie = StringUtils.replace(cookie, ";Path=/", "");
+                serverIds.add(cookie);
+                serverId = cookie;
+            }
 
-                String aaa=PACKAGELIST_URL + i+"";
-                Response res = ConnectionUtil.doGetWithLeastParams(PACKAGELIST_URL + i + "", COOKIE);
+            for (int j = 1; j <= total; j++) {
+                for (String sid : serverIds) {
 
-                html = res.returnContent().asString();
-                Document doc = Jsoup.parse(html);
+                    serverId = sid;
+                    String url = PACKAGEPAGE_URL + j;
+                    Response res2 = ConnectionUtil.doGetWithLeastParams(url, COOKIE);
+                    String content = res2.returnContent().asString();
+                    Document doc = Jsoup.parse(content);
 
-                String trRegEx = "#enable_table > div > div > table > tbody > tr";
-                int trSize = WebClientUtil.getTagSize(doc, trRegEx, HtmlTag.trName);
+                    String trRegEx = "#package_data_table_enable_1 > div > table > tbody > tr";
+                    int trSize = WebClientUtil.getTagSize(doc, trRegEx, HtmlTag.trName);
 
-                String ssss = "";
+                    if (trSize > 0) {
+                        for (int i = 2; i <= trSize; i++) {
+
+                            String memberCardNameRegEx = trRegEx + ":nth-child(" + i + ") > td:nth-child(1)";
+                            String cardIdRegEx = trRegEx + ":nth-child(" + i + ") > td:nth-child(6) > a.btn.btn-primary.btn-minier";
+                            String cardIdStr = doc.select(cardIdRegEx).attr("onclick");
+                            String getIdRegEx = "(?<=')(.*)(?=')";
+                            String cardId = CommonUtil.fetchString(cardIdStr, getIdRegEx);
+
+                            String memberCardName = doc.select(memberCardNameRegEx).text();
+
+                            MemberCard memberCard = new MemberCard();
+                            memberCard.setMemberCardName(memberCardName);
+                            memberCard.setMemberCardId(cardId);
+                            memberCards.add(memberCard);
+
+                        }
+                    }
+                }
+
+                String sss = "";
 
             }
         }
-
-        String sss = "";
 
 
     }
